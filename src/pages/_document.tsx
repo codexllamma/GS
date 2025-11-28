ced579df12cd7421c92108fc16d674e3e5f41c61
codexllamma <codexllamma@gmail.com>
2025-11-29 03:23:28 +0530

    favico

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* REMOVE this if it exists: <link rel="icon" href="/favicon.ico" /> */}
+        
+        {/* ADD THIS: Link to your new PNG in the public folder */}
+        <link rel="icon" href="/icon.png" type="image/png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
@@ -11,4 +16,3 @@ export default function Document() {
     </Html>
   );
 }
-
