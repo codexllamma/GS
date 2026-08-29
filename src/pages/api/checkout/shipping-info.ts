284d06cea4e7298b21776bb42733a68edcd02fb4
codexllamma <codexllamma@gmail.com>
2026-08-29 15:45:34 +0530

    h

--- src/pages/api/checkout/shipping-info.ts
+++ src/pages/api/checkout/shipping-info.ts
@@ -63,15 +63,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     });
 
     // Option 2: Cash on Delivery (₹50 COD Fee)
+    // Option 2: Cash on Delivery (₹59 Fee)
     if (isCodAvailable) {
       shippingMethods.push({
         id: "cod_standard",
         name: "Cash on Delivery",
         description: "Pay cash upon delivery",
         serviceable: isServiceable && isCodAvailable,
-        shipping_fee: 5900, // 5000 paise = ₹50 (Displays "₹50" badge)
-        cod: true,
-        cod_fee: 0,
+        shipping_fee: 0,     // Base shipping cost (0 if free shipping)
+        cod: true,           // Unlocks the COD payment option
+        cod_fee: 5900,       // ₹59 in paise (The extra COD surcharge)
       });
     }
 
@@ -111,9 +112,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
               name: "Cash on Delivery",
               description: "Pay cash upon delivery",
               serviceable: true,
-              shipping_fee: 5900, // ₹50
+              shipping_fee: 0, // ₹50
               cod: true,
-              cod_fee: 0,
+              cod_fee: 5900,
             },
           ],
         },
